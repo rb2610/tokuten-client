64663ec3264225646b9fbc6f19149974011b93c5
rb2610 <rb2610@hotmail.co.uk>
2018-11-18 22:15:00 +0000

    Add player button

--- src/TestTable.tsx
+++ src/TestTable.tsx
@@ -13,13 +13,17 @@ interface ITestScore {
 
 interface IState {
   data: ITestScore[];
+  formName: string;
+  formWins: number;
 }
 
 const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
 class TestTable extends React.Component<any, IState> {
   public state = {
-    data: Array<ITestScore>()
+    data: Array<ITestScore>(),
+    formName: "",
+    formWins: 0
   };
 
   constructor(props: any) {
@@ -27,12 +31,7 @@ class TestTable extends React.Component<any, IState> {
   }
 
   public componentDidMount() {
-    axios.get(`${apiUrl}/api/scoreData`).then(response => {
-      const responseData: ITestScore[] = response.data.data;
-      if(responseData) {
-        this.setState({ data: responseData });
-      }
-    });
+    this.getData();
   }
 
   public render() {
@@ -41,37 +40,79 @@ class TestTable extends React.Component<any, IState> {
     const rowsCount = this.state.data.length;
 
     return (
-      <Table
-        className={"test-table"}
-        rowHeight={rowHeight}
-        headerHeight={headerHeight}
-        rowsCount={rowsCount}
-        width={this.props.width * 0.85}
-        height={Math.min(
-          /* Margin: */ 2 + headerHeight + rowsCount * rowHeight,
-          Math.max(300, this.props.height * 0.85)
-        )}
-      >
-        <Column
-          columnKey="name"
-          header={<Cell>Name</Cell>}
-          cell={this.cellNameMapping}
-          flexGrow={1}
-          width={50}
-        />
-        <Column
-          columnKey="wins"
-          header={<Cell>Wins</Cell>}
-          cell={this.cellWinsMapping}
-          flexGrow={1}
-          width={50}
-        />
-      </Table>
+      <div>
+        <form onSubmit={this.onSubmit(this)}>
+          <input
+            id="new-player-name-field"
+            type="text"
+            value={this.state.formName}
+            placeholder="New Name"
+            onChange={this.onNameChange(this)}
+          />
+          <input id="new-player-submit" type="submit" value="Add Player" />
+        </form>
+        <Table
+          className={"test-table"}
+          rowHeight={rowHeight}
+          headerHeight={headerHeight}
+          rowsCount={rowsCount}
+          width={this.props.width * 0.85}
+          height={Math.min(
+            /* Margin: */ 2 + headerHeight + rowsCount * rowHeight,
+            Math.max(300, this.props.height * 0.85)
+          )}
+        >
+          <Column
+            columnKey="name"
+            header={<Cell>Name</Cell>}
+            cell={this.cellNameMapping}
+            flexGrow={1}
+            width={50}
+          />
+          <Column
+            columnKey="wins"
+            header={<Cell>Wins</Cell>}
+            cell={this.cellWinsMapping}
+            flexGrow={1}
+            width={50}
+          />
+        </Table>
+      </div>
     );
   }
 
+  private getData() {
+    axios.get(`${apiUrl}/api/scoreData`).then(response => {
+      const responseData: ITestScore[] = response.data.data;
+
+      if (responseData) {
+        this.setState({ data: responseData });
+      }
+    });
+  }
+
+  private handleNameChange(event: any, context: TestTable) {
+    if (event.target) {
+      context.setState({ formName: event.target.value });
+    }
+  }
+
+  private onNameChange(context: TestTable) {
+    return (event: any) => this.handleNameChange(event, context);
+  }
+
+  private handleSubmit(event: any, context: TestTable) {
+    event.preventDefault();
+    axios
+      .post(`${apiUrl}/api/scoreData`, { name: this.state.formName, wins: 0 })
+      .then(() => this.getData());
+  }
+
+  private onSubmit(context: TestTable) {
+    return (event: any) => this.handleSubmit(event, context);
+  }
+
   private cellNameMapping = (props: any) => {
-    // const row = this.state.data[props.rowIndex];
     return (
       <Cell className={`tt-name-${props.rowIndex}`}>
         {this.state.data.length > 0 ? this.state.data[props.rowIndex].name : ""}
--- test/acceptance/App.atest.ts
+++ test/acceptance/App.atest.ts
@@ -1,14 +1,40 @@
+// import chai from "chai";
+import * as mockttp from "mockttp";
 import { NightwatchBrowser } from "nightwatch";
 
+const mockServer = mockttp.getLocal();
+// const expect = chai.expect;
+
+const testHeaders = { "access-control-allow-origin": "*" };
+
 module.exports = {
-  'Test': (client : NightwatchBrowser) => {
+  Test: (client: NightwatchBrowser) => {
     client.url(client.launch_url).pause(500);
     client.expect.element("body").to.be.present;
     client.expect.element(".main-title").text.to.contain("Tokuten");
     client.end();
   },
 
-  'Test Table Test': (client: NightwatchBrowser) => {
+  "Test Table Test": (client: NightwatchBrowser) => {
+    mockServer.get("/api/scoreData").thenReply(
+      200,
+      JSON.stringify({
+        data: [
+          {
+            id: 1,
+            name: "Foo",
+            wins: 5
+          },
+          {
+            id: 2,
+            name: "Roo",
+            wins: 2
+          }
+        ]
+      }),
+      testHeaders
+    );
+
     client.url(client.launch_url).pause(500);
     client.expect.element(".test-table").to.be.present;
     client.expect.element(".tt-name-0").text.to.equal("Foo");
@@ -16,6 +42,90 @@ module.exports = {
     client.expect.element(".tt-name-1").text.to.equal("Roo");
     client.expect.element(".tt-wins-1").text.to.equal("2");
     client.end();
+  },
+
+  "Add Player Test": async (client: NightwatchBrowser) => {
+    mockServer.get("/api/scoreData").thenReply(
+      200,
+      JSON.stringify({
+        data: [
+          {
+            id: 1,
+            name: "Foo",
+            wins: 5
+          },
+          {
+            id: 2,
+            name: "Roo",
+            wins: 2
+          }
+        ]
+      }),
+      testHeaders
+    );
+    // mockServer.options("/api/scoreData").thenReply()
+    const requestMock = await mockServer.post("/api/scoreData").thenReply(
+      200,
+      JSON.stringify({
+        data: [
+          {
+            id: 3,
+            name: "Bob",
+            wins: 0
+          }
+        ]
+      }),
+      testHeaders
+    );
+    /*mockServer.get("/api/scoreData").once().thenReply(
+      200,
+      JSON.stringify({
+        data: [
+          {
+            id: 1,
+            name: "Foo",
+            wins: 5
+          },
+          {
+            id: 2,
+            name: "Roo",
+            wins: 2
+          },
+          {
+            id: 3,
+            name: "Bob",
+            wins: 0
+          }
+        ]
+      }),
+      testHeaders
+    );*/
+
+    client.url(client.launch_url).pause(500);
+    client.setValue("#new-player-name-field", "Bob");
+    client.click("#new-player-submit");
+    client.expect.element(".test-table").to.be.present;
+    /*client.expect.element(".tt-name-0").text.to.equal("Foo");
+    client.expect.element(".tt-wins-0").text.to.equal("5");
+    client.expect.element(".tt-name-1").text.to.equal("Roo");
+    client.expect.element(".tt-wins-1").text.to.equal("2");
+    client.expect.element(".tt-name-2").text.to.equal("Bob");
+    client.expect.element(".tt-wins-2").text.to.equal("0");*/
+    client.end();
+    const requests = await requestMock.getSeenRequests();
+    // tslint:disable-next-line:no-console
+    console.log("Request:");
+    // tslint:disable-next-line:no-console
+    console.log(requests);
+    // expect(requests.pop().body.json).to.deep.equal({});
+  },
+
+  beforeEach: (browser: any) => {
+    mockServer.start(3001);
+  },
+
+  afterEach: (browser: any) => {
+    mockServer.stop();
   }
 };
 
